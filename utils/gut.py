82f1d4b8189727b64c13d26327197d3cde31b0fd
dharmendra <dharmendradiwaker12@gmail.com>
2024-06-09 15:21:31 +0530

    new commit

--- utils/gut.py
+++ utils/gut.py
@@ -1,2 +1,16 @@
 def hello(name):
     return "hello" + name
+
+
+def calcule(age):
+    if age > 18:
+        return 'person is adult'
+    
+    elif age<18:
+        return 'Person is under age'
+    
+    else:
+        return 'person is 18year old'
+    
+
+print(calcule(25))

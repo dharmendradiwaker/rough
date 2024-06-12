b5df77955e24c9578ced32ec1898cd921c3c0e2f
dharmendra <dharmendradiwaker12@gmail.com>
2024-06-12 13:49:47 +0530

    upgrade

--- new.py
+++ new.py
@@ -7,24 +7,20 @@ def get_number():
     n = 50
     random_number = random.randint(0, n-1)
     return random_number
-
-def save_to_file(filename, number):
-    with open(filename, 'a') as file:
-        file.write(f"Random number: {number} - {datetime.now()}\n")
-
-def save_job():
-    filename = "C:/Users/DHARMENDRA/Desktop/test.txt"
-    number = get_number()
-    save_to_file(filename, number)
+   
 
 def main():
+    number_list = []
     while True:
         if pycron.is_now('* * * * *'):   # Every minute
             print('running')
-            save_job()
+            new_number = get_number()
+            number_list.append(new_number)
+            print("List of numbers:", number_list)
             time.sleep(60)
         else:
             time.sleep(15)
+
 
 
 if __name__ == '__main__':
